4dc532c5fccdc091a235cf9b69a47d18c31b8e86
CPYCoding <cpy.coding.504@gmail.com>
2026-05-01 23:00:01 +0800

    Upload the results of experiment A,

--- lunar_lander.py
+++ lunar_lander.py
@@ -10,14 +10,14 @@ import csv
 from functions import print_stats, plot_baseline, record_episodes, plot_training_curves, save_checkpoint
 
 # Hyperparameters (you should experiment with these!)
-LEARNING_RATE = 5e-4
+LEARNING_RATE = 1e-4
 GAMMA = 0.99  # Discount factor
 EPSILON_START = 1.0
 EPSILON_END = 0.01
-EPSILON_DECAY = 0.997
+EPSILON_DECAY = 0.995
 BATCH_SIZE = 64
-BUFFER_SIZE = 50000
-TARGET_UPDATE_FREQ = 10  # Update target network every N episodes
+BUFFER_SIZE = 10000
+TARGET_UPDATE_FREQ = 5  # Update target network every N episodes
 
 SEED = 42
 random.seed(SEED)
@@ -229,13 +229,11 @@ for episode in range(num_episodes):
             episode_losses.append(loss)
 
         # TODO: Update target network periodically
-        if episode % TARGET_UPDATE_FREQ == 0:
-            agent.update_target_network()
-        
         state = next_state
         episode_reward += reward
 
-        # pass
+    if episode % TARGET_UPDATE_FREQ == 0:
+            agent.update_target_network()
     
     # TODO: Decay epsilon
     epsilon = max(
